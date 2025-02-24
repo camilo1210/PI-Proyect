1b0af506bcedb68bcb56099850d697952c257f24
maurxio <165192098+maurxio@users.noreply.github.com>
2025-02-23 20:28:33 -0500

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
-  import "./Home.css";
+import React from 'react';
+import imagenIcono from '../../assets/d3177929-c131-46e3-a2d8-cfdedf4f5966.jpg';
 
-
-  const Home = () => {
+const Home = () => {
     return (
-      <div>
-        <h1>Inicio</h1>
-      </div>
+        <div>
+            <h1>Bienvenido a la página principal</h1>
+            <img src={imagenIcono} alt="Icono de la página" className="icono" />
+        </div>
     );
-  };
-  
+};
 
-  export default Home;
+export default Home;
+
