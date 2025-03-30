87b0900dfe7fa9cdfc5b7733ca6e5065186d88e5
Camilo Pavas <camilopavasrios@gmail.com>
2025-03-30 13:42:33 -0500

    Configuraciones Basicas
    1. Se configuro el main para acceder a la ruta de App
    
    2. se configuro App para ingresar a la pagina del sindrome del corazon roto
    
    3. se creo el archivo de variables.css para manejar los colores de manera globla
    
    4. se organizaron los estilos css

---
+++ src/pages/Broken_heart_syndrome/BrokenHeartSyndrome.jsx
@@ -0,0 +1,13 @@
+
+
+const BrokenHeartSyndrome = () =>{
+    return(
+        <div>
+            <h1>
+                Pagina del sindrome del corazon roto 
+            </h1>
+        </div>
+    )
+}
+
+export default BrokenHeartSyndrome ; 
