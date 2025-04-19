052a9634b2d70244473c3722efd278e1df047dab
maurxio <javier.mauricio.ortiz@correounivalle.edu.co>
2025-04-18 21:00:37 -0500

    agregacion de estilos css a miocardiopatiadilatada

--- src/pages/dilated-cardiomyopathy/DilatedCardiomyopathy.jsx
+++ src/pages/dilated-cardiomyopathy/DilatedCardiomyopathy.jsx
@@ -3,14 +3,32 @@ import "./DilatedCardiomyopathy.css";
 const DilatedCardiomyopathy = () => {
   return (
     <div className="dilated-cardiomyopathy">
-      <h1>Miocardiopatia Dilatada</h1>
-      <p>
-      La miocardiopatía dilatada es una enfermedad del músculo cardíaco que provoca el agrandamiento
-       de las cavidades del corazón y una disminución en su capacidad para bombear sangre de forma eficiente. 
-       Esta condición puede causar fatiga,  dificultad para respirar y, en casos graves, insuficiencia cardíaca.</p>
+      <h1>Miocardiopatía Dilatada</h1>
+
+      
+      <div class="card left">
+  <div class="title">¿QUE ES?</div>
+  <p>La miocardiopatía dilatada (MCD) es una enfermedad del músculo cardíaco en la cual el ventrículo izquierdo, que es la principal cámara de bombeo del corazón, se agranda (dilata) y pierde fuerza para contraerse y bombear sangre de forma eficiente al resto del cuerpo. Con el tiempo, esto puede afectar a otras cámaras del corazón y causar insuficiencia cardíaca.</p>
+</div>
+
+<div class="card right">
+  <div class="title">¿CUALES SON SUS SINTOMAS?</div>
+  <p>La causa puede variar, desde enfermedades del corazón hasta condiciones crónicas como la hipertensión. Un diagnóstico adecuado es esencial.</p>
+</div>
+
+<div class="card left">
+  <div class="title">¿Cuáles son sus síntomas?</div>
+  <p>Los síntomas más comunes incluyen dificultad para respirar, fatiga, hinchazón en las piernas, y latidos cardíacos irregulares. Estos signos suelen empeorar con el tiempo si no se recibe tratamiento adecuado.</p>
+</div>
+
+<div class="card right">
+  <div class="title">¿Cuál es la causa?</div>
+  <p>La causa puede variar, desde enfermedades del corazón hasta condiciones crónicas como la hipertensión. Un diagnóstico adecuado es esencial.</p>
+</div>
+
+
     </div>
   );
 };
 
 export default DilatedCardiomyopathy;
-// Este componente es una página que describe la miocardiopatía dilatada.
