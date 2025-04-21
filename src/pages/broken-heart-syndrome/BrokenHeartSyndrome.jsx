b6bc542b8fd2d36fd4f7036c3eb7788193a13b64
Camilo Pavas <camilopavasrios@gmail.com>
2025-04-20 23:02:05 -0500

    Pagina Sindrome del corazon roto
    Se organizo los primeros pasos de la apariencia de la pagina

--- src/pages/broken-heart-syndrome/BrokenHeartSyndrome.jsx
+++ src/pages/broken-heart-syndrome/BrokenHeartSyndrome.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+
+import "./BrokenHeartSyndrome.css";
 
 const BrokenHeartSyndrome = () => {
-    return (
-        <div>
-            <h1>Síndrome del Corazón Roto</h1>
-        </div>
+return (
+    <div className="broken-heart-container">
+  <h1 className="broken-heart-title">Síndrome del corazón roto</h1>
+
+  <div className="broken-heart-section">
+    <div className="broken-heart-bubble left">¿Qué es?</div>
+    <p className="broken-heart-text">
+    El síndrome del corazón roto es una afección cardíaca que a menudo se debe a situaciones estresantes y emociones extremas. La afección también puede ocasionarse por una enfermedad física grave o una cirugía. El síndrome del corazón roto suele ser temporal. Sin embargo, algunas personas pueden seguir sintiéndose mal después de que el corazón se cure.
+    </p>
+  </div>
+
+  <div className="broken-heart-section reverse">
+    <div className="broken-heart-bubble right">¿Cuáles son sus síntomas?</div>
+    <p className="broken-heart-text">
+    Las personas con síndrome del corazón roto pueden tener dolor repentino en el pecho o pensar que están teniendo un ataque cardíaco. El síndrome del corazón roto afecta solo una parte del corazón. Interrumpe brevemente la forma en que el corazón bombea sangre. El resto del corazón sigue funcionando como de costumbre. A veces, el corazón se contrae con más fuerza.
+
+    Los síntomas del síndrome del corazón roto pueden parecerse a los de un ataque cardíaco. Estos son algunos posibles síntomas:
+
+    1. Dolor en el pecho.
+    2. Falta de aire.
+    </p>
+  </div>
+
+  <div className="broken-heart-section">
+    <div className="broken-heart-bubble left">¿Qué lo causa?</div>
+    <p className="broken-heart-text">
+    Se cree que un aumento repentino de las hormonas del estrés, como la adrenalina, podría causarle un daño temporal al corazón de algunas personas en respuesta a un evento estresante emocional o físico.
+
+    El síndrome del corazón roto puede desencadenarse por situaciones estresantes, como:
+    1. La muerte de un ser querido.
+    2. Un diagnóstico grave.
+    3. Una ruptura o separación.
+    4. Estrés emocional intenso.
+    5. Estrés físico intenso, como una enfermedad grave o una cirugía.
+    </p>
+  </div>
+
+  <div className="broken-heart-section reverse">
+    <div className="broken-heart-bubble right">¿Cómo tratarlo?</div>
+    <p className="broken-heart-text">
+    El tratamiento para el síndrome del corazón roto, también conocido como miocardiopatía de Takotsubo, depende de la gravedad de los síntomas. El tratamiento es similar al de un ataque cardíaco. Puede incluir medicamentos para ayudar a aliviar el dolor en el pecho y la falta de aire. En algunos casos, es posible que necesite tratamiento para afecciones cardíacas subyacentes, como insuficiencia cardíaca o arritmias.
+
+    Tratamiento 
+
+    1. Analgésicos para aliviar el dolor
+    2. Betabloqueadores para reducir la frecuencia cardíaca
+    3. Aspirina para mejorar la circulación y prevenir coágulos de sangre
+    4. Inhibidores de la enzima convertidora de angiotensina o bloqueadores de los receptores de angiotensina para reducir la presión arterial
+    5. Diuréticos para disminuir la acumulación de líquidos
+    6. Fármacos inotrópicos para mejorar la contractilidad en casos graves
+    7. Dispositivos de asistencia ventricular en casos de shock cardiogénico
+
+    Recuperación 
+
+    La mayoría de las personas se recuperan por completo en aproximadamente un mes se realiza un ecocardiograma para asegurarse de que el corazón esté funcionando mejor.
+
+    A veces en algunos casos , el síndrome del corazón roto vuelve a aparecer después del tratamiento
+    </p>
+  </div>
+</div>
+
+
     );
 };
 
