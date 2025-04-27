bdceee0e9632df5b7482e5ba25c27050d5c182bb
Camilo Pavas <camilopavasrios@gmail.com>
2025-04-26 21:32:02 -0500

    Implementacion de un modelo 3D
    Modelos 3D
    luz
    controles
    modelo 1 en la pagina de la enfermedad

--- src/pages/broken-heart-syndrome/BrokenHeartSyndrome.jsx
+++ src/pages/broken-heart-syndrome/BrokenHeartSyndrome.jsx
@@ -1,70 +1,139 @@
-
-import "./BrokenHeartSyndrome.css";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { ContactShadows, OrbitControls } from "@react-three/drei";
+import Lights from "./lights/Lights";
+import { Model } from "./models-3d/BrokenHeartModel";
 
 const BrokenHeartSyndrome = () => {
-return (
+  return (
     <div className="broken-heart-container">
-  <h1 className="broken-heart-title">Síndrome del corazón roto</h1>
+      <h1 className="broken-heart-title">Síndrome del corazón roto</h1>
 
-  <div className="broken-heart-section">
-    <div className="broken-heart-bubble left">¿Qué es?</div>
-    <p className="broken-heart-text">
-    El síndrome del corazón roto es una afección cardíaca que a menudo se debe a situaciones estresantes y emociones extremas. La afección también puede ocasionarse por una enfermedad física grave o una cirugía. El síndrome del corazón roto suele ser temporal. Sin embargo, algunas personas pueden seguir sintiéndose mal después de que el corazón se cure.
-    </p>
-  </div>
+      <div className="model-container">
+        <Canvas
+          shadows
+          // Selecciona el tipo de shadowMap:
+          gl={{
+            antialias: true,
+            // Para SOMBRAS SUAVES:
+            shadowMap: { type: THREE.PCFSoftShadowMap },
+            // Para SOMBRAS DURAS, reemplaza por THREE.BasicShadowMap
+          }}
+          style={{
+            width: "100%",
+            height: 300,
+            background: "transparent",
+          }}
+        >
+          {/* Opción: plano invisible que solo recibe sombras */}
+          <ContactShadows
+            position={[0, -0.75, 0]} // justo debajo del modelo
+            rotation-x={-Math.PI / 2} // acostado horizontal
+            width={10}
+            height={10}
+            far={1} // distancia máxima del drop
+            resolution={512}
+            // blur = 0 → sombra dura
+            // blur > 0 → sombra más suave
+            blur={2}
+            opacity={0.5}
+          />
 
-  <div className="broken-heart-section reverse">
-    <div className="broken-heart-bubble right">¿Cuáles son sus síntomas?</div>
-    <p className="broken-heart-text">
-    Las personas con síndrome del corazón roto pueden tener dolor repentino en el pecho o pensar que están teniendo un ataque cardíaco. El síndrome del corazón roto afecta solo una parte del corazón. Interrumpe brevemente la forma en que el corazón bombea sangre. El resto del corazón sigue funcionando como de costumbre. A veces, el corazón se contrae con más fuerza.
+          <Lights />
+          <Model scale={3} position={[0, 0, 0]} />
 
-    Los síntomas del síndrome del corazón roto pueden parecerse a los de un ataque cardíaco. Estos son algunos posibles síntomas:
+          <OrbitControls
+            enableZoom
+            maxDistance={10}
+            minDistance={2}
+            autoRotate
+            autoRotateSpeed={1}
+          />
+        </Canvas>
+      </div>
 
-    1. Dolor en el pecho.
-    2. Falta de aire.
-    </p>
-  </div>
+      <div className="cards-container">
+        <div className="card left">
+          <div className="title">¿Qué es?</div>
+          <p>
+            El síndrome del corazón roto es una afección cardíaca que a menudo
+            se debe a situaciones estresantes y emociones extremas. También
+            puede ocasionarse por una enfermedad física grave o una cirugía. El
+            síndrome del corazón roto suele ser temporal, pero algunas personas
+            pueden seguir sintiéndose mal después de que el corazón se cure.
+          </p>
+        </div>
 
-  <div className="broken-heart-section">
-    <div className="broken-heart-bubble left">¿Qué lo causa?</div>
-    <p className="broken-heart-text">
-    Se cree que un aumento repentino de las hormonas del estrés, como la adrenalina, podría causarle un daño temporal al corazón de algunas personas en respuesta a un evento estresante emocional o físico.
+        <div className="card right">
+          <div className="title">¿Cuáles son sus síntomas?</div>
+          <p>
+            Las personas con síndrome del corazón roto pueden experimentar dolor
+            repentino en el pecho o pensar que están teniendo un ataque
+            cardíaco. Afecta solo una parte del corazón e interrumpe brevemente
+            la forma en que bombea sangre, mientras el resto del corazón sigue
+            funcionando. A veces, el corazón se contrae con más fuerza.
+            <br />
+            <br />
+            Los síntomas incluyen:
+            <br />
+            1. Dolor en el pecho.
+            <br />
+            2. Falta de aire.
+          </p>
+        </div>
 
-    El síndrome del corazón roto puede desencadenarse por situaciones estresantes, como:
-    1. La muerte de un ser querido.
-    2. Un diagnóstico grave.
-    3. Una ruptura o separación.
-    4. Estrés emocional intenso.
-    5. Estrés físico intenso, como una enfermedad grave o una cirugía.
-    </p>
-  </div>
+        <div className="card left">
+          <div className="title">¿Qué lo causa?</div>
+          <p>
+            Se cree que un aumento repentino de hormonas del estrés, como la
+            adrenalina, puede dañar temporalmente el corazón. Este síndrome
+            puede desencadenarse por:
+            <br />
+            1. La muerte de un ser querido.
+            <br />
+            2. Un diagnóstico grave.
+            <br />
+            3. Una ruptura o separación.
+            <br />
+            4. Estrés emocional intenso.
+            <br />
+            5. Estrés físico intenso, como una enfermedad grave o cirugía.
+          </p>
+        </div>
 
-  <div className="broken-heart-section reverse">
-    <div className="broken-heart-bubble right">¿Cómo tratarlo?</div>
-    <p className="broken-heart-text">
-    El tratamiento para el síndrome del corazón roto, también conocido como miocardiopatía de Takotsubo, depende de la gravedad de los síntomas. El tratamiento es similar al de un ataque cardíaco. Puede incluir medicamentos para ayudar a aliviar el dolor en el pecho y la falta de aire. En algunos casos, es posible que necesite tratamiento para afecciones cardíacas subyacentes, como insuficiencia cardíaca o arritmias.
-
-    Tratamiento 
-
-    1. Analgésicos para aliviar el dolor
-    2. Betabloqueadores para reducir la frecuencia cardíaca
-    3. Aspirina para mejorar la circulación y prevenir coágulos de sangre
-    4. Inhibidores de la enzima convertidora de angiotensina o bloqueadores de los receptores de angiotensina para reducir la presión arterial
-    5. Diuréticos para disminuir la acumulación de líquidos
-    6. Fármacos inotrópicos para mejorar la contractilidad en casos graves
-    7. Dispositivos de asistencia ventricular en casos de shock cardiogénico
-
-    Recuperación 
-
-    La mayoría de las personas se recuperan por completo en aproximadamente un mes se realiza un ecocardiograma para asegurarse de que el corazón esté funcionando mejor.
-
-    A veces en algunos casos , el síndrome del corazón roto vuelve a aparecer después del tratamiento
-    </p>
-  </div>
-</div>
-
-
-    );
+        <div className="card right">
+          <div className="title">¿Cómo tratarlo?</div>
+          <p>
+            El tratamiento depende de la gravedad de los síntomas y es similar
+            al de un ataque cardíaco. Puede incluir:
+            <br />
+            1. Analgésicos para aliviar el dolor.
+            <br />
+            2. Betabloqueadores para reducir la frecuencia cardíaca.
+            <br />
+            3. Aspirina para mejorar la circulación y prevenir coágulos.
+            <br />
+            4. Inhibidores de la ECA o bloqueadores de los receptores de
+            angiotensina para reducir la presión arterial.
+            <br />
+            5. Diuréticos para disminuir la acumulación de líquidos.
+            <br />
+            6. Fármacos inotrópicos para mejorar la contractilidad en casos
+            graves.
+            <br />
+            7. Dispositivos de asistencia ventricular en casos de shock
+            cardiogénico.
+            <br />
+            <br />
+            Recuperación: La mayoría de las personas se recuperan por completo
+            en aproximadamente un mes, realizando un ecocardiograma para
+            asegurar el buen funcionamiento del corazón. A veces, el síndrome
+            puede reaparecer después del tratamiento.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BrokenHeartSyndrome;
---
+++ src/pages/broken-heart-syndrome/lights/Lights.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/no-unknown-property */
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Lights = () => {
+  const spotLightRef = useRef();
+
+  const heartbeat = (t) => {
+    const bpm = 60;
+    const beatTime = 60 / bpm;
+    const cycle = t % (beatTime * 2);
+    if (cycle < beatTime * 0.2 || (cycle > beatTime * 0.5 && cycle < beatTime * 0.7)) {
+      return 1 + Math.sin((cycle % (beatTime * 0.2)) * Math.PI / (beatTime * 0.2)) * 0.5;
+    }
+    return 1;
+  };
+
+  useFrame(({ clock }) => {
+    const spot = spotLightRef.current;
+    if (!spot) {return};
+    const t = clock.getElapsedTime();
+    const beat = heartbeat(t);
+    const r = 1.5;
+    spot.position.set(Math.cos(t * 0.5) * r, 3, Math.sin(t * 0.5) * r);
+    spot.target.position.set(0, 0, 0);
+    spot.target.updateMatrixWorld();
+    spot.intensity = 2.5 * beat;
+  });
+
+  return (
+    <>
+      {/* Luz ambiental básica */}
+      <ambientLight intensity={1} />
+
+      {/* Luz direccional extra para rellenar sombras */}
+      <directionalLight position={[5, 5, 5]} intensity={0.3} />
+
+      {/* Spot animado en latido */}
+      <spotLight
+        ref={spotLightRef}
+        position={[0, 3, 0]}
+        color="#ff4f5e"
+        distance={10}
+        intensity={2.5}
+        angle={Math.PI / 8}
+        penumbra={0.4}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-bias={-0.0005}
+        shadow-radius={4}
+      />
+    </>
+  );
+};
+
+export default Lights;
