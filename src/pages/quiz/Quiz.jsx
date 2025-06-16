30b4709872b7bea2b02a2f8152702ccd26597106
JohnSitoToro <johnsito.toro13@gmail.com>
2025-06-15 22:56:32 -0500

    quiz

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,29 +1,86 @@
-import { useCallback } from "react";
-import useQuizStore from "../../stores/use-quiz-store";
 import "./Quiz.css";
-import { Canvas } from "@react-three/fiber";
-import Recipient from "../aortic-stenosis/models-3d/Recipient";
+import preguntas from "./preguntas";
+import { useEffect } from "react";
+import { useState } from "react";
+import Modelo3D from "./Modelo3D"; 
 
 
 const Quiz = () => {
-  const { quiz, incrementQuizProgress } = useQuizStore();
+  const [preguntaActual, setPreguntaActual] = useState(0);
+  const [puntuacion, setPuntuacion] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [respuestaSeleccionada, setRespuestaSeleccionada] = useState(null);
 
-  const handleQuizNext = useCallback(() => {
-    incrementQuizProgress();
-  }, [incrementQuizProgress]);
-  
+  function handleAnswerSubmit(correcta, e) {
+    if (correcta) setPuntuacion(puntuacion + 1);
+
+    e.target.classList.add(correcta ? "correct" : "incorrect");
+
+    if (preguntaActual === preguntas.length - 1) {
+      setIsFinished(true);
+    } else {
+      setPreguntaActual(preguntaActual + 1);
+    }
+  }
+
+
+
+  if (isFinished) return (
+    <main className="Quiz">
+      <div className="juego-terminado">
+        <span>Obtuviste {puntuacion} de {preguntas.length}</span>
+        <button onClick={() => window.location.reload()}>Volver a jugar</button>
+      </div>
+       <div className="modelo-3d">
+          <Modelo3D />
+        </div>
+    </main>
+  );
+
+
+  const handleRespuesta = (opcion, index) => {
+    setRespuestaSeleccionada(index);
+
+    if (opcion.correcta) {
+      setPuntuacion(puntuacion + 1);
+    }
+
+
+    setTimeout(() => {
+      setRespuestaSeleccionada(null);
+      if (preguntaActual + 1 < preguntas.length) {
+        setPreguntaActual(preguntaActual + 1);
+      } else {
+        setIsFinished(true);
+      }
+    }, 1000);
+  };
+
+
   return (
     <>
-    <div>
-      <h1>Quiz</h1>
-      <span>Progreso del quiz: {quiz.percentageQuizCompleted} % </span>
-      <Canvas>
-        <Recipient />
-      </Canvas>
-      <button onClick={handleQuizNext}>Siguiente</button>
-    </div>
+      <div className="quiz-container">
+        <div className="quiz-left">
+          <div className="quiz-progress">
+            <h1>❤️ Quiz Cardiovascular</h1>
+            <span>Pregunta  {preguntaActual + 1} de {preguntas.length} </span>
+          </div>
+
+          <div className="quiz-question">
+            <h2>{preguntas[preguntaActual].titulo}</h2>
+          </div>
+        </div>
+
+        <div className="quiz-right">
+          {preguntas[preguntaActual].opciones.map((respuesta) => (
+            <button key={respuesta.textoRespuesta}
+              onClick={(e) => handleAnswerSubmit(respuesta.correcta, e)}>
+              {respuesta.textoRespuesta}</button>
+          ))}
+
+        </div>
+      </div>
     </>
   );
-};
-
+}
 export default Quiz;
---
+++ src/pages/quiz/preguntas.js
@@ -0,0 +1,56 @@
+const preguntas = [
+{
+    titulo: "¿Qué desencadena comúnmente el síndrome del corazón roto?",
+    opciones: [
+        { textoRespuesta: "Consumo elevado de grasas saturadas", correcta: false },
+        { textoRespuesta: "Esfuerzo físico extremo", correcta: false },    
+        { textoRespuesta: "Estrés emocional intenso o pérdida afectiva", correcta: true },
+        { textoRespuesta: "Hipotiroidismo", correcta: false }
+    ],
+},
+{
+
+    titulo: "¿Cuál de los siguientes factores contribuye más comúnmente al desarrollo de la hipertensión arterial?",
+    opciones: [
+        { textoRespuesta: "Infecciones virales", correcta: false },
+        { textoRespuesta: "Consumo excesivo de sal", correcta: true },    
+        { textoRespuesta: "Exceso de calcio en la dieta", correcta: false },
+        { textoRespuesta: "Baja exposición al sol", correcta: false }
+    ],
+},
+{
+    titulo: "¿Cuál es una señal típica de insuficiencia cardíaca?",
+    opciones: [
+        { textoRespuesta: "Dolor agudo en el brazo izquierdo", correcta: false },
+        { textoRespuesta: "Tos seca durante el día", correcta: false },    
+        { textoRespuesta: "Hinchazón en piernas y tobillos", correcta: true },
+        { textoRespuesta: "Calambres musculares", correcta: false }
+    ],
+},
+{
+    titulo: "¿Qué ocurre en la miocardiopatía dilatada?",
+    opciones: [
+        { textoRespuesta: "Las arterias se endurecen y bloquean", correcta: false },
+        { textoRespuesta: "El corazón se contrae con demasiada fuerza", correcta: false },    
+        { textoRespuesta: "La válvula mitral se cierra prematuramente", correcta: false },
+        { textoRespuesta: "El músculo cardíaco se debilita y las cavidades se agrandan", correcta: true }
+    ],
+},
+{
+    titulo: "La estenosis aórtica se caracteriza por:",
+    opciones: [
+        { textoRespuesta: "El estrechamiento de la válvula aórtica", correcta: true },
+        { textoRespuesta: "El ensanchamiento excesivo del ventrículo derecho", correcta: false },    
+        { textoRespuesta: "La inflamación del saco pericárdico", correcta: false },
+        { textoRespuesta: "La disfunción del nodo sinoauricular", correcta: false }
+    ],
+},
+]
+
+export default preguntas;
+
+
+
+
+
+
